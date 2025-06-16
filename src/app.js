570ee49f7c2bb32cec404edfc2aae533c7ddff7a
Abiodun Sabitu <95466606+Abiodun-Sabitu@users.noreply.github.com>
2025-06-16 20:23:45 +0100

    add catch all route 404 middleware

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/errorHandler');
-
+createHttpError = require('http-errors');
 const authRoutes = require('./routes/auth');
 const blogRoutes = require('./routes/blog');
 
@@ -19,8 +19,18 @@ app.use(express.json());
 app.use(morgan('combined'));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/blogs', blogRoutes);
+app.get('/api/v1/', (req, res) => {
+  try {
+    res.status(200).json({ message: 'Welcome to the Blogging API' });
+  } catch (error) {
+    next(error);
+  }
+});
+app.use((req, res, next) => {
+  next(createHttpError(404, `this endpoint ${req.url} was not found`));
+});
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/blogs', blogRoutes);
 
 // Error handler
 app.use(errorHandler);
